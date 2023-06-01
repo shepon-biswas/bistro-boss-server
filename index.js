654faee7df4f7b1220ec69ffc25a15685cd37986
Shepon Biswas <sheponbiswas01@gmail.com>
2023-06-02 02:07:21 +0600

    Initial Server Setup & Deploy

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) =>{
+    res.send("Server is running")
+})
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is runnig on port  ${port}`)
+})
+
+
+
+
+
+
